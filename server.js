af0341c40572686cd67aab9ec15348809c454f9a
dennis <dwakahia@cytonn.com>
2022-01-31 15:32:21 +0300

    admin dashboard setup

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'frontend/dist')));
+app.use(serveStatic(path.join(__dirname, './frontend/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './frontend/dist/'))
+    res.sendFile(path.resolve(__dirname, './frontend/dist/index.html'))
 })
 const port = process.env.PORT || 3000;
 app.listen(port);
